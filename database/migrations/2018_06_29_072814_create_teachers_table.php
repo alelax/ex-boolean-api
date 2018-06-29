fb3019debb8112553f9e0b2c563e7b1f18c84a2f
alelax <alessandro.lausdei@gmail.com>
2018-06-29 09:56:25 +0200

    three migrations created: teachers, courses and students

---
+++ database/migrations/2018_06_29_072814_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->nullable($value = false);
+            $table->string('surname', 100)->nullable($value = false);
+            $table->smallInteger('age')->nullable($value = false);
+            $table->string('address', 100)->nullable($value = false);
+            $table->string('gender', 6)->nullable($value = false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
---
+++ database/migrations/2018_06_29_072844_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100)->nullable($value = false);
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('end_date')->nullable();
+            $table->integer('teacher_id')->unsigned();
+
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
